3334f5d0812791f87a6fb993b87fc29922a350ec
shortBread2329 <shortBread2329@outlook.jp>
2020-05-17 18:06:41 +0900

    url管理修正

--- CookLab/urls.py
+++ CookLab/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 import app.views as app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', app.index),    
-    path('', app.index),    
+    # path('app/', app.index),    
+    # path('', app.index),    
+    path('', include('app.urls')),    
+    
 ]
